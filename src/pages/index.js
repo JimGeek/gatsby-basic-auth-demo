62fb45ba5353d6003e6fca15617e0ebb2dabde6f
Jimit <Jimit@outlook.com>
2020-04-18 16:23:53 +0530

    Added new files as per tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,23 @@
 import React from "react"
+import { Link } from 'gatsby'
+import { getUser,isLoggedIn } from "../services/auth"
 
-export default () => <div>Hello world!</div>
+import Layout from "../components/layout"
+
+export default () => (
+    <Layout>
+        <h1>Hello {isLoggedIn() ? getUser().name : "World"}</h1>
+        <p>
+            {
+                isLoggedIn() ?
+                (<>
+                    You are logged in, so check your{" "}
+                    <Link to="/app/profile">Profile</Link>
+                </>) :
+                (<>
+                    You Should <Link to="/app/login">Login</Link> to see restricted content
+                </>)
+            }
+        </p>
+    </Layout>
+)
